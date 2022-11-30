5b232285eb41800a7f57f293cc469797a48d6605
DAF201 <daf201@blink-in.com>
2022-11-30 10:41:05 -0600

    test
    somehow passed modbus syntax check but the machine don't take it

--- src/modbus.py
+++ src/modbus.py
@@ -9,14 +9,13 @@ AVAILABLE_SERIAL_PORTS = [x.device
 
 
 class modbus_serial(serial.Serial):
-    def __init__(self, serial_port: str, HEX=False, *args, **kwargs) -> None:
+    def __init__(self, serial_port: str, *args, **kwargs) -> None:
         self.serial_port: str
         self.timeout: float
         self.baudrate: int
         self.xonxoff: bool
         self.rtscts: bool
         self.dsrdtr: bool
-        self.HEX = bool
         self.reading_thread: threading.Thread
         self.writing_thread: threading.Thread
         self.recving_buffer: bytes
@@ -26,7 +25,6 @@ class modbus_serial(serial.Serial):
         super().__init__(serial_port, *args, **kwargs)
 
         self.serial_port = serial_port
-        self.HEX = HEX
         self.is_running = False
         self.recving_buffer = ''
         self.modbus_message_pool = []
@@ -45,43 +43,75 @@ class modbus_serial(serial.Serial):
             pass
 
     def __recv(self):
+        self.__last_recv_register = ''
         while (1):
-            self.recving_buffer = self.read(128)
-            if self.recving_buffer != b'':
+            try:
+                self.recving_buffer = self.read(128)
                 hex_data = self.recving_buffer.hex()
-                if self.HEX:
-                    print(' '.join(re.findall('..', hex_data)))
-                else:
-                    print(self.recving_buffer)
-                self.modbus_message_pool.append(
-                    (hex_data[:2], hex_data[2:4], hex_data[4:-4], hex_data[-4:]))
-                self.recving_buffer = b''
+                if hex_data != '':
+                    print(hex_data)
+                if hex_data[4:8] == self.__last_recv_register:
+                    continue
+                if self.recving_buffer != b'':
+                    if not self.__modbus_check(hex_data[:-4], hex_data[-4:]):
+                        self.recving_buffer = b''
+                        continue
+                    else:
+                        self.modbus_message_pool.append(
+                            {'host_address': hex_data[:2], 'function_type': hex_data[2:4],
+                             'register_address': hex_data[4:8], 'number_of_register': hex_data[8:-4]})
+                        self.__last_recv_register = hex_data[4:8]
+                    self.recving_buffer = b''
+            except Exception as e:
+                print(e)
+                pass
 
     def __execute(self):
         while (1):
             try:
                 if len(self.modbus_message_pool) == 0:
                     continue
-                message = self.modbus_message_pool[0]
-                host = message[0]
-                function_code = message[1]
-                data = message[2]
-                crc = message[3]
-
-                if function_code == '03':
-                    target_register_address = int(data[:4], 16)
-                    number_of_registers = int(data[4:], 16)
-                    ret = []
-                    for x in range(number_of_registers):
-                        ret.append(
-                            self.data_base[str(target_register_address+x)])
-                    self.write(b'')
+                modbus_message = self.modbus_message_pool[0]
+                self.modbus_message_pool.pop(0)
+                if modbus_message['function_type'] == '03':
+                    # print('reading')
+                    ret = modbus_message['host_address'] + \
+                        modbus_message['function_type'] + \
+                        hex(2*int(
+                            modbus_message['number_of_register'], 16))[2:].zfill(2)
+                    for x in range(2*int(modbus_message['number_of_register'], 16)):
+                        ret += self.data_base[str(
+                            int(modbus_message['register_address'], 16)+x)]
+                    ret += self.__modbus_calculate(ret)
+                    ret: str
+                    ret = ret.encode()
+                    self.write(ret)
                     print(ret)
             except Exception as e:
                 print(e)
-            finally:
-                if len(self.modbus_message_pool) != 0:
-                    self.modbus_message_pool.pop(0)
+                pass
+
+    @classmethod
+    def __modbus_check(self, data: str, crc_data: str) -> str:
+        return self.__modbus_calculate(data) == crc_data
+
+    @classmethod
+    def __modbus_calculate(self, data: str) -> str:
+        def modbus_data_split(data: str, size=2) -> list:
+            return [int(data[x-size:x], 16) for x in range(size, len(data)+size, size)]
+        crc = 0xffff
+        A001H = 0xA001
+        data = modbus_data_split(data)
+        for x in data:
+            crc = crc ^ x
+            for i in range(0, 8):
+                if bin(crc)[-1:] == '1':
+                    crc = crc >> 1
+                    crc = crc ^ A001H
+                else:
+                    crc = crc >> 1
+        res = hex((crc >> 8) | (crc << 8))
+        return (res[int(len(res)/2):])
 
     def stop(self):
         self.is_running = False
@@ -90,10 +120,10 @@ class modbus_serial(serial.Serial):
         gc.collect()
 
     def __str__(self) -> str:
-        return ("""serial: %s\ntime out: %f\nbaudrate: %d\nxonxoff: %d\nrtscts: %d\ndsrdtr: %d\nHEX display: %d""" % (self.serial_port, self.timeout, self.baudrate, self.xonxoff, self.rtscts, self.dsrdtr, self.HEX))
+        return ("""serial: %s\ntime out: %f\nbaudrate: %d\nxonxoff: %d\nrtscts: %d\ndsrdtr: %d\n""" % (self.serial_port, self.timeout, self.baudrate, self.xonxoff, self.rtscts, self.dsrdtr))
 
 
-modbus_obj = modbus_serial(AVAILABLE_SERIAL_PORTS[0], HEX=True, timeout=0.15,
+modbus_obj = modbus_serial(AVAILABLE_SERIAL_PORTS[0], timeout=0.15,
                            baudrate=115200, xonxoff=False, rtscts=False, dsrdtr=False)
 print(modbus_obj)
 modbus_obj.run()
--- test/test.py
+++ test/test.py
@@ -1,124 +1,126 @@
-import serial
-import serial.tools.list_ports
-import threading
-from time import time, sleep
-import json
-AVAILABLE_SERIAL_PORTS = [x.device
-                          for x in serial.tools.list_ports.comports()]
+# import serial
+# import serial.tools.list_ports
+# import threading
+# from time import time, sleep
+# import json
+# AVAILABLE_SERIAL_PORTS = [x.device
+#                           for x in serial.tools.list_ports.comports()]
 
 
-def update_available_serial_ports():
-    global AVAILABLE_SERIAL_PORTS
-    AVAILABLE_SERIAL_PORTS = [x.device
-                              for x in serial.tools.list_ports.comports()]
+# def update_available_serial_ports():
+#     global AVAILABLE_SERIAL_PORTS
+#     AVAILABLE_SERIAL_PORTS = [x.device
+#                               for x in serial.tools.list_ports.comports()]
 
 
-class serial_port(serial.Serial):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+# class serial_port(serial.Serial):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
 
-        self.buffer = ''
-        self.last_recieved_char = ''
-        self.last_read_time = 0
-        self.last_buffer_size = 0
-        self.last_buffer_splited = False
+#         self.buffer = ''
+#         self.last_recieved_char = ''
+#         self.last_read_time = 0
+#         self.last_buffer_size = 0
+#         self.last_buffer_splited = False
 
-        self.command_pool = []
+#         self.command_pool = []
 
-        self.serial_read_thread = threading.Thread(target=self.serial_read)
-        self.serial_read_thread.daemon = True
-        self.serial_timer_thread = threading.Thread(target=self.serial_timer)
-        self.serial_timer_thread.daemon = True
+#         self.serial_read_thread = threading.Thread(target=self.serial_read)
+#         self.serial_read_thread.daemon = True
+#         self.serial_timer_thread = threading.Thread(target=self.serial_timer)
+#         self.serial_timer_thread.daemon = True
 
-        self.serial_read_thread.start()
-        self.serial_timer_thread.start()
+#         self.serial_read_thread.start()
+#         self.serial_timer_thread.start()
 
-    def serial_timer(self):
-        while (1):
-            if self.last_recieved_char != '\n':
-                self.last_recieved_char = '\n'
+#     def serial_timer(self):
+#         while (1):
+#             if self.last_recieved_char != '\n':
+#                 self.last_recieved_char = '\n'
 
-                if self.buffer != '':
-                    buffer_size = len(self.buffer)
-                    if buffer_size < 8:
-                        continue
-                    if (self.last_buffer_splited and buffer_size == self.last_buffer_size) or (buffer_size > self.last_buffer_size and buffer_size/2 == self.last_buffer_size):
-                        self.command_pool.append(self.buffer[:buffer_size//2])
-                        self.command_pool.append(self.buffer[buffer_size//2:])
-                        self.last_buffer_splited = True
-                    else:
-                        self.command_pool.append(self.buffer)
-                        self.last_buffer_splited = False
+#                 if self.buffer != '':
+#                     buffer_size = len(self.buffer)
+#                     if buffer_size < 8:
+#                         continue
+#                     if (self.last_buffer_splited and buffer_size == self.last_buffer_size) or (buffer_size > self.last_buffer_size and buffer_size/2 == self.last_buffer_size):
+#                         self.command_pool.append(self.buffer[:buffer_size//2])
+#                         self.command_pool.append(self.buffer[buffer_size//2:])
+#                         self.last_buffer_splited = True
+#                     else:
+#                         self.command_pool.append(self.buffer)
+#                         self.last_buffer_splited = False
 
-                    self.last_buffer_size = buffer_size
-                    self.buffer = ''
+#                     self.last_buffer_size = buffer_size
+#                     self.buffer = ''
 
-    def serial_read(self):
-        while (1):
-            serial_buffer = self.read(128)
-            self.buffer = self.buffer+str(serial_buffer.hex())
-            self.last_read_time = time()
-            self.last_recieved_char = serial_buffer
+#     def serial_read(self):
+#         while (1):
+#             serial_buffer = self.read(128)
+#             self.buffer = self.buffer+str(serial_buffer.hex())
+#             self.last_read_time = time()
+#             self.last_recieved_char = serial_buffer
 
-    def serial_write(self, data):
-        self.write(data)
+#     def serial_write(self, data):
+#         self.write(data)
 
-    def __del__(self) -> None:
-        return super().__del__()
-
-
-class modbus_serial_port(serial_port):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        try:
-            with open('simulator.json', 'r')as file_storage:
-                self.data_buffer = json.load(file_storage)
-        except Exception as e:
-            print(e)
-            self.data_buffer = {}
-        self.command_fetching_and_execution_thread = threading.Thread(
-            target=self.command_fetching)
-        self.command_fetching_and_execution_thread.daemon = True
-
-        self.file_update_thread = threading.Thread(
-            target=self.file_update, args=(5,))
-
-        self.command_fetching_and_execution_thread.start()
-        self.file_update_thread.start()
-
-    def file_update(self, time_interval):
-        while (1):
-            with open('simulator.json', 'w')as file_storage:
-                json.dump(self.data_buffer, file_storage)
-            sleep(time_interval)
-
-    def command_fetching(self):
-        while (1):
-            if len(self.command_pool) != 0:
-                self.command_execution()
-
-    def command_execution(self):
-        command = self.command_pool[0]
-        address = command[:2]
-        command_type = command[2:4]
-        register_address = command[4:8]
-        number_of_registers = command[8:12]
-        if command_type == '06':
-            data = command[8:16]
-        crc = command[-4:]
-        if command_type == '06' and all(map(lambda x: x != '', (address, command_type, register_address, data, crc))):
-            print('writing:', register_address,
-                  'number of registers:', number_of_registers, 'data:', data)
-            self.data_buffer[register_address] = data
-            print(self.command_pool[0])
-        else:
-            try:
-                print('reading:', register_address,
-                      'number of registers:', number_of_registers, 'value:', self.data_buffer[register_address])
-            except:
-                pass
-        self.command_pool.pop(0)
+#     def __del__(self) -> None:
+#         return super().__del__()
 
 
-modbus = modbus_serial_port(AVAILABLE_SERIAL_PORTS[0], timeout=0.15,
-                            baudrate=19200, xonxoff=False, rtscts=False, dsrdtr=False)
+# class modbus_serial_port(serial_port):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         try:
+#             with open('simulator.json', 'r')as file_storage:
+#                 self.data_buffer = json.load(file_storage)
+#         except Exception as e:
+#             print(e)
+#             self.data_buffer = {}
+#         self.command_fetching_and_execution_thread = threading.Thread(
+#             target=self.command_fetching)
+#         self.command_fetching_and_execution_thread.daemon = True
+
+#         self.file_update_thread = threading.Thread(
+#             target=self.file_update, args=(5,))
+
+#         self.command_fetching_and_execution_thread.start()
+#         self.file_update_thread.start()
+
+#     def file_update(self, time_interval):
+#         while (1):
+#             with open('simulator.json', 'w')as file_storage:
+#                 json.dump(self.data_buffer, file_storage)
+#             sleep(time_interval)
+
+#     def command_fetching(self):
+#         while (1):
+#             if len(self.command_pool) != 0:
+#                 self.command_execution()
+
+#     def command_execution(self):
+#         command = self.command_pool[0]
+#         address = command[:2]
+#         command_type = command[2:4]
+#         register_address = command[4:8]
+#         number_of_registers = command[8:12]
+#         if command_type == '06':
+#             data = command[8:16]
+#         crc = command[-4:]
+#         if command_type == '06' and all(map(lambda x: x != '', (address, command_type, register_address, data, crc))):
+#             print('writing:', register_address,
+#                   'number of registers:', number_of_registers, 'data:', data)
+#             self.data_buffer[register_address] = data
+#             print(self.command_pool[0])
+#         else:
+#             try:
+#                 print('reading:', register_address,
+#                       'number of registers:', number_of_registers, 'value:', self.data_buffer[register_address])
+#             except:
+#                 pass
+#         self.command_pool.pop(0)
+
+
+# modbus = modbus_serial_port(AVAILABLE_SERIAL_PORTS[0], timeout=0.15,
+#                             baudrate=19200, xonxoff=False, rtscts=False, dsrdtr=False)
+
+print(hex(int('a', 16)*4)[2:].zfill(4))
