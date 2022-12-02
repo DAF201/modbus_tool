99f9e5c6658b0da46d97ca429d7381115460a3c8
daf201 <daf201@blink-in.com>
2022-12-02 11:36:45 -0600

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-
-def data_split(data: str, size=2, hex=True) -> list:
-    if hex:
-        return [int(data[x-size:x], 16) for x in range(size, len(data)+size, size)]
-    else:
-        return [data[x-size:x] for x in range(size, len(data)+size, size)]
-
-
-print(data_split('0004085751524559595555', 4, hex=False))
